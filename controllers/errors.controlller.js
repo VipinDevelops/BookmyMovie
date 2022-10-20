1e7382714c3e8a33149240c999d1f44b6feaa6ef
itsvipindev <VipinChaudhary1809@gmail.com>
2022-10-20 18:33:43 +0530

    doc(controller): Enhance 404 error response

--- controllers/errors.controlller.js
+++ controllers/errors.controlller.js
@@ -1,4 +1,6 @@
 module.exports.get404 = function(error, req, res, next) {
-  res.status(404).send('Invalid input');
+  res.status(404).send('Invalid input').json({
+    message: error.message,
+  });
   console.log('Error');
 };

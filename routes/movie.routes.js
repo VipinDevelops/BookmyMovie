f87407210e420e2640f58e8df2eb0cc295dd371d
itsvipindev <VipinChaudhary1809@gmail.com>
2022-10-21 15:43:23 +0530

    chore(movie.routes):removed duplicate middlewares

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -3,28 +3,19 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const movieController = require('../controllers/movie.controller');
-const middleware = require('../middlewares/VerifyToken');
 // Post
-router.post('/post', middleware.verifyToken, movieController.PostMovie);
+router.post('/post', movieController.PostMovie);
 
 // Get all
-router.get('/getall', middleware.verifyToken, movieController.GetAllMovie);
+router.get('/getall', movieController.GetAllMovie);
 
 // Get One
-router.get('/getone/:id', middleware.verifyToken, movieController.GetOneMovie);
+router.get('/getone/:id', movieController.GetOneMovie);
 
 // Update by ID
-router.patch(
-    '/update/:id',
-    middleware.verifyToken,
-    movieController.UpdateMovie,
-);
+router.patch('/update/:id', movieController.UpdateMovie);
 
 // Delete by ID
-router.delete(
-    '/delete/:id',
-    middleware.verifyToken,
-    movieController.DeleteMovie,
-);
+router.delete('/delete/:id', movieController.DeleteMovie);
 
 module.exports = router;

fa49eec8e080750331d10938d2e8fb602515ac93
itsvipindev <VipinChaudhary1809@gmail.com>
2022-10-21 21:20:57 +0530

    docs(auth.routes): add method comments

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,7 +5,13 @@ const authController = require('../controllers/auth.controller');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+// POST
 router.post('/register', authController.register);
+
+// POST
 router.post('/login', authController.login);
+
+// GET
 router.get('/me', middleware.verifyToken, authController.me);
+
 module.exports = router;

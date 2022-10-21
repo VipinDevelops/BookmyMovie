4a98f1402b47b133684bd26be02722ec133cf811
itsvipindev <VipinChaudhary1809@gmail.com>
2022-10-21 21:19:43 +0530

    docs(user.model): add JSDoc

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+/**
+ * Represents a User JSON
+ * @constructor
+ * @param {name} name - The name of the user.
+ * @param {string} email - The genre of the user.
+ * @param {string}} password - The password of the user.
+ */
 const userSchema = new Schema({
   name: {
     type: String,
